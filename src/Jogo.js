9624580a36acf152aac6ac6efa0369302061eaf9
Leticia Santos Moura <letistos@gmail.com>
2023-05-10 13:59:30 -0300

    layout inicial

---
+++ src/Jogo.js
@@ -0,0 +1,16 @@
+export default function Jogo(){
+    return(
+        <div class="main">
+            <div class="left">
+                <img src="/assets/forca0.png" alt ="forca"/>
+            </div>
+            <div class="right">
+                
+                <button class="font">Escolher Palavra</button>
+                
+                <div class="font">_ _ abcuae_ _ _ _ _ _</div>
+            </div>
+
+        </div>
+    );
+}
---
+++ src/Letras.js
@@ -0,0 +1,36 @@
+export default function Letras(){
+    return(
+
+    <div class="letter">
+        <button class="font">A</button>
+        <button class="font">B</button>
+        <button class="font">C</button>
+        <button class="font">D</button>
+        <button class="font">E</button>
+        <button class="font">F</button>
+        <button class="font">G</button>
+        <button class="font">H</button>
+        <button class="font">I</button>
+        <button class="font">J</button>
+        <button class="font">K</button>
+        <button class="font">L</button>
+        <button class="font">M</button>
+        <button class="font">N</button>
+        <button class="font">O</button>
+        <button class="font">P</button>
+        <button class="font">Q</button>
+        <button class="font">R</button>
+        <button class="font">S</button>
+        <button class="font">T</button>
+        <button class="font">U</button>
+        <button class="font">V</button>
+        <button class="font">W</button>
+        <button class="font">X</button>
+        <button class="font">Y</button>
+        <button class="font">Z</button>
+
+     </div>
+
+    );
+}
+
